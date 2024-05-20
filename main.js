e57fb48c3e809533dc5ad09011644247992f26cc
Muhammad Ikhsan Fauzi <137757092+IkhsanFreya@users.noreply.github.com>
2024-05-20 19:24:48 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+function clicks() {
+  $("#div1").hide();
+  $("#div2").show();
+  $("#div2").addClass("fadeinv2");
+
+  var typed = new Typed(".katakata", {
+    strings: [
+      "haloo cantikk, apaa kabar?",
+      "cuma mau bilang...",
+      "Semangat terus yaaa kuliahnya. aku tau kamu capee, tapi kamu udah keren banget udah bisa lewatin semuanyaaa!!!!",
+      "Jangan lupa makann sama air putihnyaa jugaaa yaaaa",
+      "Jangan sampai kamu capeekk apalagi sakitt:(",
+      "hihihi, semoga kamu sukaaa yaa aku bikinin kaya gini",
+      "have a nice day  ^w^",
+    ],
+    typeSpeed: 40,
+  });
+}
+function clicks() {
+  $("#div1").hide();
+  $("#div2").show();
+  $("#div2").addClass("fadeinv2");
+
+  var typed = new Typed(".katakata", {
+    strings: [
+      "haloo cantikk, apaa kabar?",
+      "cuma mau bilang...",
+      "Semangat terus yaaa kuliahnya. aku tau kamu capee, tapi kamu udah keren banget udah bisa lewatin semuanyaaa!!!!",
+      "Jangan lupa makann sama air putihnyaa jugaaa yaaaa",
+      "Jangan sampai kamu capeekk apalagi sakitt:(",
+      "hihihi, semoga kamu sukaaa yaa aku bikinin kaya gini",
+      "have a nice day  ^w^ ",
+    ],
+    typeSpeed: 40,
+  });
+}
